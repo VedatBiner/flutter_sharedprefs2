7adecf346a818c9da0b27a1b7fc6085642403295
Vedat Biner <vbiner@gmail.com>
2022-08-17 15:08:03 +0300

    sharedprefs-1

--- lib/theme_data.dart
+++ lib/theme_data.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 ThemeData green = ThemeData(
     primarySwatch: Colors.green,
@@ -10,6 +11,7 @@ ThemeData red = ThemeData(
 
 class ThemeColorData with ChangeNotifier{
 
+  late SharedPreferences _sharedPrefObject;
   bool _isGreen = true;
 
   //getter
@@ -21,7 +23,35 @@ class ThemeColorData with ChangeNotifier{
   }
 
   void toggleTheme(){
-    _isGreen= !_isGreen;
+    _isGreen = !_isGreen;
+    saveThemeToSharedPref(_isGreen);
     notifyListeners();
+  }
+
+  Future<void> createSharedPrefObject() async {
+    _sharedPrefObject = await SharedPreferences.getInstance();
+  }
+
+  // yazma metodu
+  void saveThemeToSharedPref(bool value){
+    _sharedPrefObject.setBool("themeData", value);
+  }
+
+  // okuma metodu
+  Future<void> loadThemeFromSharedPref() async {
+    await createSharedPrefObject();
+
+    _isGreen = _sharedPrefObject.getBool("themeData")?? true;
+
+    /*
+    if(_sharedPrefObject.getBool("themeData")! == null){
+      _isGreen = true;
+    } else {
+      _isGreen = _sharedPrefObject.getBool("themeData")!;
+    }
+
+     */
+
+
   }
 }
